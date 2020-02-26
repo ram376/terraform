5a71568c770231ed5ed9d5542e6b06402092cf1d
ram376 <57516587+ram376@users.noreply.github.com>
2020-02-26 16:59:18 +0530

    Create terra-ec2.tf

---
+++ terra-ec2.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "ec2-insta"{
+ami = "ami-026839cf0f1f3928d"
+instance_type = "t2.micro"
+subnet_id = "subnet-5069af18"
+iam_instance_profile = "ssm-ec2role"
+user_data = <<-EOT
+             #!/bin/bash
+             sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+             sudo systemctl start amazon-ssm-agent
+EOT
+tags = {
+  Name = "ec2-insta"
+}
+}
+terraform {
+  backend "s3" {
+    bucket = "terra-s3-ram"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
